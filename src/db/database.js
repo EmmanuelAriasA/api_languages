150037ae52adfb810ff3e761ca13332bb7b64a83
EmmanuelAriasA <mane.ariasa@gmail.com>
2023-10-04 22:35:00 -0600

    Cambios en la API

--- src/db/database.js
+++ src/db/database.js
@@ -5,6 +5,7 @@ import config from "../config";
 const connection = mysql.createConnection({
   host: config.host,
   database: config.database,
+  port: config.port,
   user: config.user,
   password: config.password,
 });
